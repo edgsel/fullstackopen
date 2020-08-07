4340e5439923ecba7f235330114cf679d2c6652d
edgsel <edgar.selihov@gmail.com>
2020-08-07 17:53:40 +0300

    1.3 solved

--- part1/courseinfo/1.2/src/Content.js
+++ part1/courseinfo/1.2/src/Content.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Part from "./Part";
 
 const Content = (props) => {
+    console.log(props);
     return(
         <>
-            <Part parts={props.parts[0]} exercises={props.exercises[0]}/>
-            <Part parts={props.parts[1]} exercises={props.exercises[1]}/>
-            <Part parts={props.parts[2]} exercises={props.exercises[2]}/>
+            <Part parts={props.parts[0].name} exercises={props.parts[0].exercises}/>
+            <Part parts={props.parts[1].name} exercises={props.parts[1].exercises}/>
+            <Part parts={props.parts[2].name} exercises={props.parts[2].exercises}/>
         </>
     )
 };
