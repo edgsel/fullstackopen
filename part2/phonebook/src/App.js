d228fb2fa1a45c66450c71f0154779afaa1dbafc
edgsel <edgar.selihov@gmail.com>
2020-08-24 20:38:40 +0300

    finish 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,39 +2,48 @@ import React, {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        {name: 'Arto Hellas', phoneNumber: '040-1234567'}
     ]);
-    const [newName, setNewName] = useState('');
 
-    const addNewName = (event) => {
+    const [newName, setNewName] = useState('');
+    const [newPhoneNumber, setNewPhoneNumber] = useState('');
+
+    const addNewPerson = (event) => {
         event.preventDefault();
         persons.some(person => person.name === newName)
             ? alert(`${newName} is already added to phonebook`)
-            : setPersons(persons.concat({name: newName}));
+            : setPersons(persons.concat({name: newName, phoneNumber: newPhoneNumber}));
 
         setNewName('');
+        setNewPhoneNumber('');
     };
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
     };
 
+    const handlePhoneNumberChange = (event) => {
+        setNewPhoneNumber(event.target.value);
+    };
+
     return (
         <div>
-            <div>debug: {newName}</div>
             <h2>Phonebook</h2>
-            <form onSubmit={addNewName}>
+            <form onSubmit={addNewPerson}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange}/>
+                </div>
+                <div>
+                    number: <input value={newPhoneNumber} onChange={handlePhoneNumberChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => <p key={person.name}>{person.name}</p>)}
+            {persons.map(person => <p key={person.name}>{person.name} {person.phoneNumber}</p>)}
         </div>
     )
 };
 
-export default App
+export default App;
